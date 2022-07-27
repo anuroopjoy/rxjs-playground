7e4336743af870947f4678fc9e655e9a65c84314
Anuroop <anuroopjoy@gmail.com>
2022-07-27 11:15:54 +0530

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,35 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'rxjs-playground';
+  currentIndex = 0;
+
+  items = [
+    'ajax',
+    'bindCallback',
+    'bindNodeCallback',
+    'defer',
+    'empty',
+    'from',
+    'fromEvent',
+    'fromEventPattern',
+    'generate',
+    'interval',
+    'of',
+    'range',
+    'throwError',
+    'timer',
+    'iif',
+  ];
+
+  trackByFn(index: number) {
+    return index;
+  }
+
+  setActive(index: number) {
+    this.currentIndex = index;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CreationComponent } from './creation/creation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreationComponent
   ],
   imports: [
     BrowserModule
